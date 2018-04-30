a71438e841e2dfd983b7399b0b66084f38da6db1
mcerovic <milutin.cerovic@gmail.com>
2018-04-30 09:31:51 +0200

    WIP: CV

---
+++ source/09_Computer_Vision_Intro/read_web_cam.py
@@ -0,0 +1,21 @@
+import cv2
+
+# http://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_gui/py_video_display/py_video_display.html
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow('frame', gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
